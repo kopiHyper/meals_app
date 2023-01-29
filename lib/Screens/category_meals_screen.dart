f651d795d57379a7fa5a349f0f176b2349ba7ab2
kopiHyper <oktawiankopica2001@gmail.com>
2023-01-29 20:48:00 +0100

    Styled the meal card showed under each category

--- lib/Screens/category_meals_screen.dart
+++ lib/Screens/category_meals_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/Models/meal.dart';
+import 'package:meals_app/Widgets/meal_item.dart';
 
 import '../Data/dummy_data.dart';
 
@@ -27,7 +27,13 @@ class CategoryMealsScreen extends StatelessWidget {
         ),
         body: ListView.builder(
             itemBuilder: (ctx, index) {
-              return Text(categoryMeals[index].title);
+              return MealItem(
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                duration: categoryMeals[index].duration,
+                complexity: categoryMeals[index].complexity,
+                affordability: categoryMeals[index].affordability,
+              );
             },
             itemCount: categoryMeals.length));
   }
