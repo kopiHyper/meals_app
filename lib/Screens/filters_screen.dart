8038e3050048992b7b92085a41752b7dd4c8c830
kopiHyper <oktawiankopica2001@gmail.com>
2023-02-05 20:27:17 +0100

    Fixed infinity page stack for better performance

--- lib/Screens/filters_screen.dart
+++ lib/Screens/filters_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../Widgets/main_drawer.dart';
+
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Filters'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your filters'),
+      ),
+      drawer: MainDrawer(),
+      body: const Center(
+        child: Text('Filters'),
+      ),
     );
   }
 }
